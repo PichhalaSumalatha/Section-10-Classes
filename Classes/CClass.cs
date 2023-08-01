e051f8b43a2a4824528ce0e7c41428079791731d
lovely suma <sumalatha11@gmail.com>
2023-08-01 19:59:40 +0530

    first commit

---
+++ Classes/CClass.cs
@@ -0,0 +1,38 @@
+﻿using Classes;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Classes
+{
+    public class CClass
+    {
+        private string name;
+        private Teacher classTeacher;
+        private List<Student> students;
+        private List<Subject> subjects;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public Teacher ClassTeacher
+        {
+            get { return classTeacher; }
+            set { classTeacher = value; }
+        }
+
+        public List<Student> Students
+        {
+            get { return students; }
+            set { students = value; }
+        }
+
+        public List<Subject> Subjects
+        {
+            get { return subjects; }
+            set { subjects = value; }
+        }
+    }
+}
